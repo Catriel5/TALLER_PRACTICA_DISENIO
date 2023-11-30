8f9996357efc757ab979a4600d5e08df950dde10
Catriel5 <143026260+Catriel5@users.noreply.github.com>
2023-11-30 04:10:23 -0300

    Delete PROYECTO FINAL- EVENTOS Y HOTEL/evento_hotel directory

--- PROYECTO FINAL- EVENTOS Y HOTEL/evento_hotel/evento_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class EventoAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'evento_app'
--- PROYECTO FINAL- EVENTOS Y HOTEL/evento_hotel/evento_app/tests.py
+++
@@ -1,9 +0,0 @@
-# evento_app/tests.py
-from django.test import TestCase
-from .models import Evento
-
-class EventoModelTest(TestCase):
-    def test_evento_model(self):
-        evento = Evento(nombre="Evento de prueba", fecha="2023-01-01", descripcion="Descripción de prueba")
-        self.assertEqual(str(evento), evento.nombre)
-
